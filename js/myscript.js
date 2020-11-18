4271106dea9028025f107ebaa00fa955f15c8e8b
Anna <rosalenanna@gmail.com>
2020-11-18 18:36:40 +0100

    funzione rimuovi

--- js/myscript.js
+++ js/myscript.js
@@ -6,13 +6,16 @@ var app= new Vue({
     logo:"img/logo-piccolo.png",
     todo:"",
     lista:[
-
     ]
-
   },
   methods: {
     aggiungi: function () {
       this.lista.push(this.todo);
+    },
+
+    rimuovi: function(item){
+      this.lista.splice(this.lista.indexOf(item),1);
     }
+
   }
 });
